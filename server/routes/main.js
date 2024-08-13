6b0edb27586a7e3f231e26fccc60a9842b5f94ab
Jimmy Yen <ichthyoid95@gmail.com>
2024-08-13 15:34:36 +0800

    template, css and mongodb

--- server/routes/main.js
+++ server/routes/main.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const Post = require('../models/Post');
 
+/**
+ * GET /
+ * HOME
+ */
 router.get('', (req, res) => {
-    res.send("Hello World")
+    const locals = {
+        title: "Nodejs Blog",
+        description: "Simple Blog created with Nodejs, Express & Mongodb"
+    }
+    res.render('index', { locals });
+});
+
+router.get('/about', (req, res) => {
+    res.render('about')
 })
 
 module.exports = router;
